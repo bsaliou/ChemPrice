85de823aa8a8e07a67e5dca7ed345d05be40d7d2
bsaliou <baptiste1saliou@gmail.com>
2023-08-29 13:31:34 +0200

    api

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,15 +45,14 @@ intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 
 # -- Options for HTML output
-
 html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
 
-# Add logo
-#html_logo = "images/logo_chemprice.png"
-#html_theme_options = {
-#    'logo_only': True,
-#    'display_version': False,
-#}
+#Add logo
+html_logo = "images/logo_chemprice.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
