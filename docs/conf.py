19503fe604d5505be47bfa746a1f4d16627e9080
bsaliou <baptiste1saliou@gmail.com>
2023-08-29 13:13:15 +0200

    api

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,8 @@
 # Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup --------------------------------------------------------------
 
@@ -9,53 +13,78 @@
 import os
 import sys
 
-# Ajouter le deuxième chemin d'accès
 sys.path.insert(0, os.path.abspath('..'))
 
-# Ajouter le deuxième chemin d'accès
-sys.path.insert(0, os.path.abspath('../chemicalprices'))
 
+# -- Project information -----------------------------------------------------
 
-# -- Project information
-
-project = 'Chemical Price Research'
-copyright = '2023, Baptiste Saliou'
+project = 'ChemPrice'
+copyright = '2021, Baptiste Saliou'
 author = 'Baptiste Saliou'
 
-release = '0.1'
-version = '0.1.0'
+import subprocess
+commit_id = subprocess.check_output(['git', 'describe', '--tags', '--abbrev=0']).strip().decode('ascii')
+version = commit_id
+# The full version, including alpha/beta/rc tags.
+release = version
 
-# -- General configuration
 
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
     "sphinx.ext.mathjax",
     "sphinx.ext.viewcode"
 ]
 
-
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
-
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# -- Options for HTML output
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst', '.md']
+source_suffix = ".rst"
 
+# The master toctree document.
+master_doc = "index"
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = "sphinx"
+
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = False
+
+from datetime import date
+today = date.today()
+today = today.strftime("%b %d, %Y")
+rst_prolog = f"""
+.. |today| replace:: {today}
+"""
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
 html_theme = 'sphinx_rtd_theme'
 
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
 
 # Add logo
-#html_logo = "images/logo_chemprice.png"
-#html_theme_options = {
-#    'logo_only': True,
-#    'display_version': False,
-#}
+html_logo = "images/logo_chemprice.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
