1407331129ca09bf27902f9038f96e254c42b9e6
Baptiste <baptiste.saliou@etu.unistra.fr>
2024-01-10 01:46:12 +0100

    new config

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="chemprice", 
-    version="1.0.1",
+    version="1.0.2",
     author="Baptiste SALIOU, Murat Cihan Sorkun, Suleyman Er",
     author_email="baptiste1saliou@gmail.com, mcsorkun@gmail.com",
     description="A python library for chemical price Search.",
